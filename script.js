ad14966164afc805177201822fadffaa57c4c858
Aletxi <pablo.aracena.m@gmail.com>
2024-09-27 20:31:33 -0300

    cambios en el repositorio 1

--- script.js
+++ script.js
@@ -4,26 +4,31 @@ let selectedCards = [];
 let valuesUsed = [];
 let currentMove = 0;
 let currentAttempts = 0;
+let pairsFound = 0;
 
 let cardTemplate = '<div class="card"><div class="back"></div><div class="face"></div></div>';
+const totalPairs = totalCards / 2; // Número total de pares
 
 function activate(e) {
    if (currentMove < 2) {
       
-      if ((!selectedCards[0] || selectedCards[0] !== e.target) && !e.target.classList.contains('active') ) {
+      if ((!selectedCards[0] || selectedCards[0] !== e.target) && !e.target.classList.contains('active')) {
          e.target.classList.add('active');
          selectedCards.push(e.target);
 
          if (++currentMove == 2) {
-
             currentAttempts++;
             document.querySelector('#stats').innerHTML = currentAttempts + ' intentos';
 
             if (selectedCards[0].querySelectorAll('.face')[0].innerHTML == selectedCards[1].querySelectorAll('.face')[0].innerHTML) {
                selectedCards = [];
                currentMove = 0;
-            }
-            else {
+               pairsFound++;
+               // Verifica si se han encontrado todos los pares
+               if (pairsFound === totalPairs) {
+                  showPopup(); // Muestra el popup cuando se gana
+               }
+            } else {
                setTimeout(() => {
                   selectedCards[0].classList.remove('active');
                   selectedCards[1].classList.remove('active');
@@ -41,14 +46,12 @@ function randomValue() {
    let values = valuesUsed.filter(value => value === rnd);
    if (values.length < 2) {
       valuesUsed.push(rnd);
-   }
-   else {
+   } else {
       randomValue();
    }
 }
 
-
-for (let i=0; i < totalCards; i++) {
+for (let i = 0; i < totalCards; i++) {
    let div = document.createElement('div');
    div.innerHTML = cardTemplate;
    cards.push(div);
@@ -56,4 +59,13 @@ for (let i=0; i < totalCards; i++) {
    randomValue();
    cards[i].querySelectorAll('.face')[0].innerHTML = valuesUsed[i];
    cards[i].querySelectorAll('.card')[0].addEventListener('click', activate);
+}
+
+function showPopup() {
+   const popup = document.getElementById('popup');
+   popup.style.display = 'block';
+
+   document.getElementById('restartButton').addEventListener('click', () => {
+      window.location.reload(); // Reinicia la página
+   });
 }

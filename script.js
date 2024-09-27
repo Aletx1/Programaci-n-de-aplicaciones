340aa8fd1511eb529602498887b284ad8a93a13d
Aletxi <pablo.aracena.m@gmail.com>
2024-09-27 20:46:54 -0300

    cambios finales al card game

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const totalCards = 12;
+const totalCards = 12; 
 let cards = [];
 let selectedCards = [];
 let valuesUsed = [];
@@ -6,8 +6,18 @@ let currentMove = 0;
 let currentAttempts = 0;
 let pairsFound = 0;
 
-let cardTemplate = '<div class="card"><div class="back"></div><div class="face"></div></div>';
 const totalPairs = totalCards / 2; // Número total de pares
+const imageFolder = 'imagenes/'; // Carpeta donde están las imágenes
+const imagePaths = [
+    "C:/Users/palo-/Desktop/uni/progra aplicaciones/lab/guess the card/imagenes/primera.jpg",
+    "C:/Users/palo-/Desktop/uni/progra aplicaciones/lab/guess the card/imagenes/segunda.jpg",
+    "C:/Users/palo-/Desktop/uni/progra aplicaciones/lab/guess the card/imagenes/tercera.jpg",
+    "C:/Users/palo-/Desktop/uni/progra aplicaciones/lab/guess the card/imagenes/cuarta.jpg",
+    "C:/Users/palo-/Desktop/uni/progra aplicaciones/lab/guess the card/imagenes/quinta.jpg",
+    "C:/Users/palo-/Desktop/uni/progra aplicaciones/lab/guess the card/imagenes/sexta.jpg"
+]; // Array para almacenar las rutas de las imágenes
+
+let cardTemplate = '<div class="card"><div class="back"></div><div class="face"></div></div>';
 
 function activate(e) {
    if (currentMove < 2) {
@@ -20,10 +30,14 @@ function activate(e) {
             currentAttempts++;
             document.querySelector('#stats').innerHTML = currentAttempts + ' intentos';
 
-            if (selectedCards[0].querySelectorAll('.face')[0].innerHTML == selectedCards[1].querySelectorAll('.face')[0].innerHTML) {
+            const img1 = selectedCards[0].querySelector('.face img').src;
+            const img2 = selectedCards[1].querySelector('.face img').src;
+
+            if (img1 === img2) {
                selectedCards = [];
                currentMove = 0;
                pairsFound++;
+
                // Verifica si se han encontrado todos los pares
                if (pairsFound === totalPairs) {
                   showPopup(); // Muestra el popup cuando se gana
@@ -42,7 +56,7 @@ function activate(e) {
 }
 
 function randomValue() {
-   let rnd = Math.floor(Math.random() * totalCards * 0.5);
+   let rnd = Math.floor(Math.random() * (totalCards * 0.5));
    let values = valuesUsed.filter(value => value === rnd);
    if (values.length < 2) {
       valuesUsed.push(rnd);
@@ -51,15 +65,34 @@ function randomValue() {
    }
 }
 
+function loadImages() {
+   for (let i = 1; i <= totalPairs; i++) {
+      imagePaths.push(`${imageFolder}image${i}.jpg`); // Carga las imágenes con nombres como 'image1.jpg', 'image2.jpg', etc.
+   }
+}
+
+function assignImagesToCards() {
+   valuesUsed.forEach((value, index) => {
+      const imgElement = document.createElement('img');
+      imgElement.src = imagePaths[value];
+      cards[index].querySelector('.face').appendChild(imgElement); // Asigna la imagen correspondiente
+   });
+}
+
+// Carga las imágenes
+loadImages();
+
 for (let i = 0; i < totalCards; i++) {
    let div = document.createElement('div');
    div.innerHTML = cardTemplate;
    cards.push(div);
    document.querySelector('#game').append(cards[i]);
    randomValue();
-   cards[i].querySelectorAll('.face')[0].innerHTML = valuesUsed[i];
    cards[i].querySelectorAll('.card')[0].addEventListener('click', activate);
 }
+
+// Asigna las imágenes a las cartas después de haber generado los valores
+assignImagesToCards();
 
 function showPopup() {
    const popup = document.getElementById('popup');
